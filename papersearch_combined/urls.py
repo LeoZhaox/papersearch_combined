1390cce153674695ed6f525c73d9e157fcbd9f99
LeoZhaox <kuaidaoxiaozhao@gmail.com>
2021-06-08 19:23:54 +0800

    second commit

--- papersearch_combined/urls.py
+++ papersearch_combined/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject1 URL Configuration
+"""papersearch_combined URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
